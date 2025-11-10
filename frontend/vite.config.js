0ec87d37e63f075470f1ef57d363a393544d2a81
Prabesh2-004 <coder.bro2004@gmail.com>
2025-11-10 13:25:52 +0545

    Change API proxy target to production URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://todo-w1hq.onrender.com',
         changeOrigin: true,
         secure: false
       }
