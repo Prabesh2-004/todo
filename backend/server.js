9f1ec1b93e8d3e0039bda8eff69c056669f1f452
Prabesh2-004 <coder.bro2004@gmail.com>
2025-11-10 13:31:07 +0545

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://todo-frontend-gx13.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
